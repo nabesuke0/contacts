9a1dd2f9661db57df3c72242991b5d9e2d49edb2
Kazuya Watanabe <watanabe@neopa.jp>
2017-05-17 16:33:25 +0900

    ちょっと更新 Provider追加

--- app/src/main/java/jp/studio/edamame/contacts/model/Contact.kt
+++ app/src/main/java/jp/studio/edamame/contacts/model/Contact.kt
@@ -3,5 +3,11 @@ package jp.studio.edamame.contacts.model
 /**
  * Created by Watanabe@Neopa on 2017/05/17.
  */
-class Contact {
+class Contact(
+        id: Int,
+        name: String,
+        mailAddresses: Array<String>) {
+
+    var id: Int = id
+    var name: String = name
 }
---
+++ app/src/main/java/jp/studio/edamame/contacts/provider/ContactsProvider.kt
@@ -0,0 +1,7 @@
+package jp.studio.edamame.contacts.provider
+
+/**
+ * Created by Watanabe@Neopa on 2017/05/17.
+ */
+class ContactsProvider {
+}
