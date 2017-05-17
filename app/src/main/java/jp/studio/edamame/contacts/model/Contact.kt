55c1ff00ac47a714f143b2c4da702dbd8e0d23c4
Kazuya Watanabe <watanabe@neopa.jp>
2017-05-17 12:18:39 +0900

    更新

---
+++ app/src/main/java/jp/studio/edamame/contacts/model/Contact.kt
@@ -0,0 +1,7 @@
+package jp.studio.edamame.contacts.model
+
+/**
+ * Created by Watanabe@Neopa on 2017/05/17.
+ */
+class Contact {
+}
--- app/src/main/java/jp/studio/edamame/contacts/views/all/AllContactsFragment.kt
+++ app/src/main/java/jp/studio/edamame/contacts/views/all/AllContactsFragment.kt
@@ -1,5 +1,7 @@
 package jp.studio.edamame.contacts.views.all
 
+import android.os.Bundle
+import android.view.View
 import jp.studio.edamame.contacts.R
 import jp.studio.edamame.contacts.views.ViewPagerFragment
 
@@ -7,13 +9,29 @@ import jp.studio.edamame.contacts.views.ViewPagerFragment
  * Created by Watanabe@Neopa on 2017/05/16.
  */
 class AllContactsFragment: ViewPagerFragment() {
+    lateinit var viewModel: AllContactsViewModel
+
     override fun getTitle(): String {
-        return "Contacts"
+        return "全ての連絡先"
     }
 
     override fun layoutId(): Int {
         return R.layout.fragment_allcontacts
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
+        viewModel = AllContactsViewModel()
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
 }
