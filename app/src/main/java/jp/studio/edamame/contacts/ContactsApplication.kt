30d00bf10dcac5d945f613379851af3fa95d7e80
Kazuya Watanabe <watanabe@neopa.jp>
2017-05-17 19:50:59 +0900

    Applicationインスタンスアクセス

--- app/src/main/java/jp/studio/edamame/contacts/ContactsApplication.kt
+++ app/src/main/java/jp/studio/edamame/contacts/ContactsApplication.kt
@@ -6,4 +6,16 @@ import android.app.Application
  * Created by Watanabe@Neopa on 2017/05/12.
  */
 class ContactsApplication : Application() {
+    companion object {
+        private var self : ContactsApplication? = null
+        fun getApp() : ContactsApplication {
+            return self!!
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        ContactsApplication.self = this
+    }
 }
