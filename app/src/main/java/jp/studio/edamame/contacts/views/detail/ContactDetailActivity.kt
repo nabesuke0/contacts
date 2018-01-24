a575090d50481483254a742bc68b862feaedd1cf
Kazuya Watanabe <watanabe@neopa.jp>
2018-01-24 18:28:16 +0900

    詳細画面と編集画面の枠だけ追加

---
+++ app/src/main/java/jp/studio/edamame/contacts/views/detail/ContactDetailActivity.kt
@@ -0,0 +1,13 @@
+package jp.studio.edamame.contacts.views.detail
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import jp.studio.edamame.contacts.R
+
+class ContactDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_detail)
+    }
+}
